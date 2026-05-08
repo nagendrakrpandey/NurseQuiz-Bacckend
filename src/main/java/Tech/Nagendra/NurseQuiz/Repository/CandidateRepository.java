def119f8d82bb6047e4b9cf41d3ad653d6d98117
pandeynagendra <nagendra.p@urbanites.in>
2026-05-08 12:38:57 +0530

    remove opencv face service for render deploy

--- src/main/java/Tech/Nagendra/NurseQuiz/Repository/CandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package Tech.Nagendra.NurseQuiz.Repository;
-
-import Tech.Nagendra.NurseQuiz.Entity.Candidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-}
